0a8bd86cfd876417c014658eccd29da81dfd0dbb
Jess Robertson <jesslaurenrobertson@gmail.com>
2023-02-28 17:46:54 -0700

    Refactoring render function

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-
 const Didact = {
   createElement,
   render,
 };
 
-const element = Didact.createElement();
 const container = document.getElementById("root");
 const node = document.createElement(element.type);
 node["title"] = element.props.title;
@@ -35,12 +33,42 @@ function createTextElement(text) {
 }
 
 function render(element, container) {
-  const dom = element.type == "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type)
-  element.props.children.forEach(child => render(child, dom))
+  const dom =
+    element.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
 
-  container.appendChild(dom)
+  const isProperty = (key) => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name];
+    });
+  element.props.children.forEach((child) => render(child, dom));
+
+  container.appendChild(dom);
 }
-console.log(createElement("div"));
 
-node.appendChild(text);
-container.appendChild(node);
+let nextUnitOfWork = null
+function workLoop(deadline){
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield){
+    nextUnitOfWork = preformUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+
+  function preformUnitOfWork(nextUnitOfWork){
+    
+  }
+}
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+);
+
+Didact.render(element, container)
+// node.appendChild(text);
+// container.appendChild(node);
